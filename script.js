8908b2850997c15cbba9d19b3f5987a9c1cf1653
NiK <nikakarumidze@gmail.com>
2022-04-13 16:24:18 +0400

    Second carousel in development

--- script.js
+++ script.js
@@ -15,8 +15,13 @@ window.onload = () => {
         for (let x of JSON.parse(localStorage.getItem("locations"))) {
             addLocations(x);
             getCity(x)
-            .then(data => getWeather(data.Key))
-            .then(data => UpdateUI(data,x))
+            .then(data => { 
+                return [ 
+                    getWeather(data.Key)
+                        .then(data => UpdateUI(data,x)),
+                    dailyWeather(data.Key)
+                        .then(e => DailyUI(e))
+                ]})
         }
     }
     getAllCities();
@@ -30,10 +35,14 @@ const currentLocation = () => {
     .then(response => response.json())
     .then(response => response.query)
     .then(response => {
-        fetch(`http://dataservice.accuweather.com/locations/v1/cities/ipaddress?apikey=${weatherKey}&q=${response}`)
+        fetch(`https://dataservice.accuweather.com/locations/v1/cities/ipaddress?apikey=${weatherKey}&q=${response}`)
         .then(e => e.json())
-        .then(data => {HereIam = `${data.EnglishName}, ${data.Country.EnglishName}`; return getWeather(data.Key);})
-        .then(data => UpdateUI(data, HereIam, "active", "true"))
+        .then(data => {HereIam = `${data.EnglishName}, ${data.Country.EnglishName}`;
+        return [getWeather(data.Key)
+            .then(e => UpdateUI(e, HereIam, "active", "true")),
+        dailyWeather(data.Key)
+            .then(e => DailyUI(e, "active"))
+    ];})
     })
 }
 
@@ -41,17 +50,14 @@ const currentLocation = () => {
 
 const weatherContent = document.querySelector("#weather_content_1");
 function UpdateUI (data, location, active, activeTrue) {
-    for (x of weatherContent.children) {
-        x.classList.remove("active")
-    }
     let metricScore;
     if(localStorage.getItem("metric") == "F") {metricScore = data[0].Temperature.Imperial.Value}
     else {metricScore = data[0].Temperature.Metric.Value}
-    var textColor = "text-dark";
+    let textColor = "text-dark";
     if(window.localStorage.getItem("theme") === "dark"){textColor = "text-light"};
 
     var newDiv = document.createElement("div");
-    newDiv.classList.add("carousel-item", "active");
+    newDiv.classList.add("carousel-item");
     newDiv.innerHTML =`
     <div class="carousel-caption ${textColor}">
         <h1 class="CtoF">&deg;<span>${metricScore}</span></h1>
@@ -71,6 +77,7 @@ function UpdateUI (data, location, active, activeTrue) {
         }
         newbutton.setAttribute("aria-current", `${activeTrue}`);
         newbutton.setAttribute("class", `${active}`);
+        newDiv.classList.add("active");
     }
     document.querySelector("#carouselFirst .carousel-indicators").appendChild(newbutton);
 }
@@ -167,18 +174,33 @@ searchBar.addEventListener("submit", e => {
 
     if (searchBar.search_add.value.trim()){
         getCity(searchBar.search_add.value.trim())
-        .then(data => {addLocations(`${data.EnglishName}, ${data.Country.EnglishName}`); return getWeather(data.Key)})
-        .then(data => {
-            UpdateUI(data, locations.lastChild.firstChild.nextSibling.innerHTML, "active", "true");
-            var saveLocations = document.querySelectorAll(".locations span");
-            localArr =[];
-            saveLocations.forEach((e, index) => {
-                if(index > 0) {
-                    localArr.push(e.textContent);
-                }
-            })
-            localStorage.setItem("locations", JSON.stringify(localArr));})
-        .then(() => searchBar.reset())
+        .then(data => {addLocations(`${data.EnglishName}, ${data.Country.EnglishName}`);
+        return [
+            getWeather(data.Key)
+                .then(data => {
+                    for (x of weatherContent.children) {
+                        x.classList.remove("active")
+                    }
+                    UpdateUI(data, locations.lastChild.firstChild.nextSibling.innerHTML, "active", "true");
+                    var saveLocations = document.querySelectorAll(".locations span");
+                    localArr =[];
+                    saveLocations.forEach((e, index) => {
+                        if(index > 0) {
+                            localArr.push(e.textContent);
+                        }
+                    })
+                    localStorage.setItem("locations", JSON.stringify(localArr));})
+                .then(() => searchBar.reset())
+                ,
+            dailyWeather(data.Key)
+                .then(e => {
+                    for (let x of secondCarousel.children) {
+                        x.classList.remove("active")
+                    };
+                    DailyUI(e, "active")
+                })   
+        ]})
+        
         ;
         }
 })
@@ -230,6 +252,14 @@ const getWeather = async id => {
     return data;
 }
 
+// Getting Weather forecast - 1 day
+const dailyWeather = async dataKey => {
+    const hourlyLink = `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${dataKey}?apikey=${weatherKey}`;
+    const hourlyResponse = await fetch(hourlyLink);
+    const hourlyData = await hourlyResponse.json();
+    return hourlyData;
+}
+
 // City Information
 const getCity = async city => {
     const baseplusApiKey = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${weatherKey}&q=${city}`;
@@ -279,3 +309,41 @@ document.querySelector("#carouselFirst .carousel-control-prev").addEventListener
        
     }
 } )
+
+const secondCarousel = document.querySelector("#carousel_second_content");
+
+// Daily forecast HTML
+function DailyUI (data, active) {
+    let metricScore2;
+    let metricScore3;
+    if(localStorage.getItem("metric") == "C") {metricScore2 = Math.round((data.DailyForecasts[0].Temperature.Minimum.Value-32)*5/9 * 10) / 10; metricScore3 = Math.round((data.DailyForecasts[0].Temperature.Maximum.Value-32)*5/9 * 10) / 10}
+    else {metricScore2 = data.DailyForecasts[0].Temperature.Minimum.Value; metricScore3 = data.DailyForecasts[0].Temperature.Maximum.Value }
+
+    let createDiv = document.createElement("div");
+    createDiv.classList.add("carousel-item");
+    let textColor1 = "text-dark";
+    if(window.localStorage.getItem("theme") === "dark"){textColor1 = "text-light"};
+
+    if (active == "active") {createDiv.classList.add("active")}
+
+    createDiv.innerHTML = `
+    <div class="d-flex justify-content-around pt-2">
+        <div>
+            <h6 class="${textColor1}">Day</h6>
+            <img src="icons/${data.DailyForecasts[0].Day.Icon}.svg" alt="weather-icon-day">
+            <p>${data.DailyForecasts[0].Day.IconPhrase}</p>
+            <p class="CtoF">&deg;<span>${metricScore3}</span> </p>
+        </div>
+        <div>
+            <h6 class="${textColor1}">Night</h6>
+            <img src="icons/${data.DailyForecasts[0].Night.Icon}.svg" alt="weather-icon-night">
+            <p>${data.DailyForecasts[0].Night.IconPhrase}</p>
+            <p class="CtoF">&deg;<span>${metricScore2}</span> </p>
+        </div>
+    </div>
+    <div>
+        <h5 class="${textColor1} p-2"> ${data.Headline.Text} </h5>
+    </div>
+    `;
+    secondCarousel.appendChild(createDiv);
+}
