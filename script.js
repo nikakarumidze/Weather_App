19cf50eacc5628278fceea94a7498f742c0270bd
NiK <nikakarumidze@gmail.com>
2022-04-14 17:13:28 +0400

    geolocation added

--- script.js
+++ script.js
@@ -35,10 +35,27 @@ window.onload = () => {
     }
     getAllCities();
 };
+// in case http request is blocked, we use geolocation API
+function geoLocationCoords (position) {
+    fetch(`https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${weatherKey}&q=${position.coords.latitude}, ${position.coords.longitude}`)
+    .then(e => e.json())
+    .then( data => {
+        const HereIam = `${data.EnglishName}, ${data.Country.EnglishName}`;
+        return [
+            getWeather(data.Key)
+                .then(e => updateUI(e, HereIam, "active", "true")) ,
+            dailyWeather(data.Key)
+                .then(e => dailyUI(e, "active"))
+            ];
+        });
+}
 
 // Current Location API
 const currentLocation = () => {
     fetch('http://ip-api.com/json?fields=8192')
+        .catch(e => {
+            navigator.geolocation.getCurrentPosition(geoLocationCoords)
+        })
     .then(response => response.json())
     .then(response => response.query)
     .then(response => {
